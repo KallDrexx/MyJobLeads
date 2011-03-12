6d1bf5a7e95c0d9ac18ab005ff347a223a2f453b
mshapiro <kalldrexx@gmail.com>
2011-03-12 11:43:29 -0500

    Added the User entity, PasswordUtils class and unit tests

---
+++ MyJobLeads.DomainModel/Utilities/PasswordUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevOne.Security.Cryptography.BCrypt;
+
+namespace MyJobLeads.DomainModel.Utilities
+{
+    public class PasswordUtils
+    {
+        /// <summary>
+        /// Generates a password hash from the credentials specified
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>Hash value for the password</returns>
+        public static string CreatePasswordHash(string username, string password)
+        {
+            // Concat username and password together, and generate a password hash from that
+            return BCryptHelper.HashPassword(username + password, BCryptHelper.GenerateSalt(15));
+        }
+
+        /// <summary>
+        /// Check's if the specified credentials are valid for the specified password hash value
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="hash"></param>
+        /// <returns>True if the credentials match the hash</returns>
+        public static bool CheckPasswordHash(string username, string password, string hash)
+        {
+            return BCryptHelper.CheckPassword(username + password, hash);
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Utilities/PasswordUtilsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Utilities;
+
+namespace MyJobLeads.Tests.Utilities
+{
+    [TestClass]
+    public class PasswordUtilsTests
+    {
+        [TestMethod]
+        public void Correct_User_Credentials_Passes_Password_Check()
+        {
+            // Setup
+            string username = "user name";
+            string password = "passWord";
+            string hash = "$2a$15$qPHP3aj9z3j/6f/4BGwuUeuIgWYqidQ/OxrXEayXVoc1RC5s9rLse";
+
+            // Act
+            bool result = PasswordUtils.CheckPasswordHash(username, password, hash);
+
+            // Verify 
+            Assert.AreEqual(true, result, "Password check failed");
+        }
+
+        [TestMethod]
+        public void Incorrect_User_Credentials_Fails_Password_Check()
+        {
+            // Setup
+            string username = "user name";
+            string password = "passWord";
+            string hash = "$2a$15$qPHP3aj9z3j/6f/4BGwuUeuIgWYqidQ/OxrXEayXVoc1RC5s9rLse";
+
+            // Act
+            bool result = PasswordUtils.CheckPasswordHash(username, password, hash);
+
+            // Verify 
+            Assert.AreEqual(false, result, "Password check passed but should have failed");
+        }
+    }
+}
