0ee8a4dff68a3970e10c879ec6724aa03567721d
mshapiro <kalldrexx@gmail.com>
2011-03-19 17:26:57 -0400

    Created the TasksByContactId query and tests

---
+++ MyJobLeads.DomainModel/Queries/Tasks/TasksByContactIdQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+
+namespace MyJobLeads.DomainModel.Queries.Tasks
+{
+    /// <summary>
+    /// Query that retrieves tasks associated with a contact
+    /// </summary>
+    public class TasksByContactIdQuery
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected int _contactId;
+
+        public TasksByContactIdQuery(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the Contact to retrieve tasks for
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        public TasksByContactIdQuery WithContactId(int contactId)
+        {
+            _contactId = contactId;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        /// <returns></returns>
+        public IList<Task> Execute()
+        {
+            return _unitOfWork.Tasks.Fetch().Where(x => x.Contact.Id == _contactId).ToList();
+        }
+    }
+}
