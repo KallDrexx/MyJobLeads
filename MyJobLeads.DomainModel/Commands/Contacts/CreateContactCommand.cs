fd1fe40f78998ba7764e6e34210e6f8eb55704c2
mshapiro <kalldrexx@gmail.com>
2011-03-19 00:40:48 -0400

    Created the CreateContactCommand class and tests

---
+++ MyJobLeads.DomainModel/Commands/Contacts/CreateContactCommand.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.Companies;
+
+namespace MyJobLeads.DomainModel.Commands.Contacts
+{
+    /// <summary>
+    /// Command class for creating a new contact
+    /// </summary>
+    public class CreateContactCommand
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected int _companyId;
+        protected string _name, _directPhone, _mobilePhone, _ext, _email, _assistant, _referredBy, _notes;
+
+        public CreateContactCommand(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the company to create the contact for
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        public CreateContactCommand WithCompanyId(int companyId)
+        {
+            _companyId = companyId;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the name for the contact
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the contact's direct phone number
+        /// </summary>
+        /// <param name="directPhone"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetDirectPhone(string directPhone)
+        {
+            _directPhone = directPhone;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the contact's mobile phone number
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetMobilePhone(string mobilePhone)
+        {
+            _mobilePhone = mobilePhone;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the contact's extension
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetExtension(string extension)
+        {
+            _ext = extension;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the contact's email
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetEmail(string email)
+        {
+            _email = email;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the name of the contact's assistant
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetAssistant(string assistant)
+        {
+            _assistant = assistant;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies who the contact was referred by
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetReferredBy(string referredBy)
+        {
+            _referredBy = referredBy;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the notes for the contact
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public CreateContactCommand SetNotes(string notes)
+        {
+            _notes = notes;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="MJLEntityNotFoundException">Thrown when the company is not found</exception>
+        public Contact Execute()
+        {
+            // Retrieve the company
+            var company = new CompanyByIdQuery(_unitOfWork).WithCompanyId(_companyId).Execute();
+
+            // Create the contact
+            var contact = new Contact
+            {
+                Company = company,
+                Name = _name,
+                DirectPhone = _directPhone,
+                MobilePhone = _mobilePhone,
+                Extension = _ext,
+                Email = _email,
+                Assistant = _assistant,
+                ReferredBy = _referredBy,
+                Notes = _notes
+            };
+
+            _unitOfWork.Contacts.Add(contact);
+            _unitOfWork.Commit();
+
+            return contact;
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Commands/Contacts/CreateContactCommandTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Commands.Contacts;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Exceptions;
+
+namespace MyJobLeads.Tests.Commands.Contacts
+{
+    [TestClass]
+    public class CreateContactCommandTests : EFTestBase
+    {
+        private Company _company;
+
+        private void InitializeTestEntities()
+        {
+            _company = new Company();
+            _unitOfWork.Companies.Add(_company);
+            _unitOfWork.Commit();
+        }
+
+        [TestMethod]
+        public void Can_Create_New_Contact()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            new CreateContactCommand(_unitOfWork).WithCompanyId(_company.Id)
+                                                 .SetName("Name")
+                                                 .SetDirectPhone("111-111-1111")
+                                                 .SetMobilePhone("222-222-2222")
+                                                 .SetExtension("33")
+                                                 .SetEmail("blah@blah.com")
+                                                 .SetAssistant("Assistant")
+                                                 .SetReferredBy("Referred By")
+                                                 .SetNotes("Notes")
+                                                 .Execute();
+
+            // Verify
+            Contact result = _unitOfWork.Contacts.Fetch().SingleOrDefault();
+            Assert.IsNotNull(result, "No contact was created");
+            Assert.AreEqual("Name", result.Name, "The contact's name was incorrect");
+            Assert.AreEqual("111-111-1111", result.DirectPhone, "The contact's direct phone was incorrect");
+            Assert.AreEqual("222-222-2222", result.MobilePhone, "The contact's mobile phone was incorrect");
+            Assert.AreEqual("33", result.Extension, "The contact's extension was incorrect");
+            Assert.AreEqual("blah@blah.com", result.Email, "The contact's email was incorrect");
+            Assert.AreEqual("Assistant", result.Assistant, "The contact's assistant was incorrect");
+            Assert.AreEqual("Referred By", result.ReferredBy, "The contact's referred by was incorrect");
+            Assert.AreEqual("Notes", result.Notes, "The contact's notes was incorrect");
+        }
+
+        [TestMethod]
+        public void Execute_Returns_Created_Contact()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            Contact result = new CreateContactCommand(_unitOfWork).WithCompanyId(_company.Id)
+                                                                 .SetName("Name")
+                                                                 .SetDirectPhone("111-111-1111")
+                                                                 .SetMobilePhone("222-222-2222")
+                                                                 .SetExtension("33")
+                                                                 .SetEmail("blah@blah.com")
+                                                                 .SetAssistant("Assistant")
+                                                                 .SetReferredBy("Referred By")
+                                                                 .SetNotes("Notes")
+                                                                 .Execute();
+
+            // Verify
+            Assert.IsNotNull(result, "No contact was created");
+            Assert.AreEqual("Name", result.Name, "The contact's name was incorrect");
+            Assert.AreEqual("111-111-1111", result.DirectPhone, "The contact's direct phone was incorrect");
+            Assert.AreEqual("222-222-2222", result.MobilePhone, "The contact's mobile phone was incorrect");
+            Assert.AreEqual("33", result.Extension, "The contact's extension was incorrect");
+            Assert.AreEqual("blah@blah.com", result.Email, "The contact's email was incorrect");
+            Assert.AreEqual("Assistant", result.Assistant, "The contact's assistant was incorrect");
+            Assert.AreEqual("Referred By", result.ReferredBy, "The contact's referred by was incorrect");
+            Assert.AreEqual("Notes", result.Notes, "The contact's notes was incorrect");
+        }
+       
+        [TestMethod]
+        public void Execute_Throws_Exception_When_JobSearch_Not_Found()
+        {
+            // Setup
+            InitializeTestEntities();
+            int id = _company.Id + 1;
+
+            // Act
+            try
+            {
+                new CreateContactCommand(_unitOfWork).WithCompanyId(id)
+                                                    .SetName("Name")
+                                                    .SetDirectPhone("111-111-1111")
+                                                    .SetMobilePhone("222-222-2222")
+                                                    .SetExtension("33")
+                                                    .SetEmail("blah@blah.com")
+                                                    .SetAssistant("Assistant")
+                                                    .SetReferredBy("Referred By")
+                                                    .SetNotes("Notes")
+                                                    .Execute();
+                Assert.Fail("Command did not throw an exception");
+            }
+
+            // Verify
+            catch (MJLEntityNotFoundException ex)
+            {
+                Assert.AreEqual(typeof(Company), ex.EntityType, "MJLEntityNotFoundException's entity type was incorrect");
+                Assert.AreEqual(id.ToString(), ex.IdValue, "MJLEntityNotFoundException's id value was incorrect");
+            }
+        }
+    }
+}
