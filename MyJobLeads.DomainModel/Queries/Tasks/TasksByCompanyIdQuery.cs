958eecb3e40d5dc29c1c1fa7313d40d7abe36d78
mshapiro <kalldrexx@gmail.com>
2011-03-19 17:17:26 -0400

    Created the TasksByCompanyId query and tests

---
+++ MyJobLeads.DomainModel/Queries/Tasks/TasksByCompanyIdQuery.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+
+namespace MyJobLeads.DomainModel.Queries.Tasks
+{
+    /// <summary>
+    /// Query class for retrieving tasks associated with a company
+    /// </summary>
+    public class TasksByCompanyIdQuery
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected int _companyId;
+
+        public TasksByCompanyIdQuery(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the company to retrieve tasks for
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        public TasksByCompanyIdQuery WithCompanyId(int companyId)
+        {
+            _companyId = companyId;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        /// <returns></returns>
+        public IList<Task> Execute()
+        {
+            return _unitOfWork.Tasks.Fetch().Where(x => x.Company.Id == _companyId).ToList();
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Queries/Tasks/TasksByCompanyIdQueryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.Tasks;
+
+namespace MyJobLeads.Tests.Queries.Tasks
+{
+    [TestClass]
+    public class TasksByCompanyIdQueryTests : EFTestBase
+    {
+        private Company _company1, _company2;
+        private Task _task1, _task2, _task3;
+
+        private void InitializeTestEntities()
+        {
+            _company1 = new Company { Tasks = new List<Task>() };
+            _company2 = new Company { Tasks = new List<Task>() };
+
+            _task1 = new Task();
+            _task2 = new Task();
+            _task3 = new Task();
+
+            _company1.Tasks.Add(_task1);
+            _company2.Tasks.Add(_task2);
+            _company1.Tasks.Add(_task3);
+
+            _unitOfWork.Companies.Add(_company1);
+            _unitOfWork.Companies.Add(_company2);
+            _unitOfWork.Commit();
+        }
+
+        [TestMethod]
+        public void Can_Retrieve_Tasks_For_Company()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            IList<Task> results = new TasksByCompanyIdQuery(_unitOfWork).WithCompanyId(_company1.Id).Execute();
+
+            // Verify
+            Assert.IsNotNull(results, "Query returned a null list");
+            Assert.AreEqual(2, results.Count, "Returned list had an incorrect number of elements");
+            Assert.AreEqual(_task1.Id, results[0].Id, "The first task had an incorrect id value");
+            Assert.AreEqual(_task3.Id, results[1].Id, "The second task had an incorrect id value");
+        }
+    }
+}
