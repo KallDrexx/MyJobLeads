c4658cec7bcc204674b1a27dbc514f2c962e7e5e
mshapiro <kalldrexx@gmail.com>
2011-03-19 18:33:05 -0400

    Created the CreateTaskForContact command and tests

---
+++ MyJobLeads.DomainModel/Commands/Tasks/CreateTaskForContactCommand.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.Contacts;
+
+namespace MyJobLeads.DomainModel.Commands.Tasks
+{
+    /// <summary>
+    /// Command class that creates a task for a contact
+    /// </summary>
+    public class CreateTaskForContactCommand
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected string _name;
+        protected DateTime? _taskDate;
+        protected int _contactId;
+
+        public CreateTaskForContactCommand(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the contact to create the task for
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        public CreateTaskForContactCommand WithContactId(int companyId)
+        {
+            _contactId = companyId;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the name of the task
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public CreateTaskForContactCommand SetName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the datetime for the task
+        /// </summary>
+        /// <param name="taskDate"></param>
+        /// <returns></returns>
+        public CreateTaskForContactCommand SetTaskDate(DateTime? taskDate)
+        {
+            _taskDate = taskDate;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="MJLEntityNotFoundException">Thrown when the specified company is not found</exception>
+        public Task Execute()
+        {
+            // Retrieve the company
+            var company = new ContactByIdQuery(_unitOfWork).WithContactId(_contactId).Execute();
+
+            // Create the Task
+            var task = new Task
+            {
+                Contact = company,
+                Name = _name,
+                TaskDate = _taskDate
+            };
+
+            _unitOfWork.Tasks.Add(task);
+            _unitOfWork.Commit();
+
+            return task;
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Commands/Tasks/CreateTaskForContactCommandTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Exceptions;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Commands.Tasks;
+
+namespace MyJobLeads.Tests.Commands.Tasks
+{
+    [TestClass]
+    public class CreateTaskForContactCommandTests : EFTestBase
+    {
+        private Contact _contact;
+        private DateTime? _testDate;
+
+        private void InitializeTestEntities()
+        {
+            _contact = new Contact { Tasks = new List<Task>() };
+            _unitOfWork.Contacts.Add(_contact);
+            _unitOfWork.Commit();
+
+            _testDate = new DateTime(2011, 1, 2, 3, 4, 5);
+        }
+
+        [TestMethod]
+        public void Can_Create_Company_Task()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            new CreateTaskForContactCommand(_unitOfWork).WithContactId(_contact.Id)
+                                                        .SetName("Name")
+                                                        .SetTaskDate(_testDate)
+                                                        .Execute();
+
+            // Verify
+            Task result = _unitOfWork.Tasks.Fetch().SingleOrDefault();
+            Assert.IsNotNull(result, "No task was created in the database");
+            Assert.AreEqual("Name", result.Name, "Task's name was incorrect");
+            Assert.AreEqual(_testDate, result.TaskDate, "Task's date value was incorrect");
+            Assert.IsFalse(result.Completed, "Task's completed status value is incorrect");
+            Assert.AreEqual(_contact.Id, result.Contact.Id, "Task's contact id value is incorrect");
+        }
+
+        [TestMethod]
+        public void Execute_Returns_Created_Task()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            Task result = new CreateTaskForContactCommand(_unitOfWork).WithContactId(_contact.Id)
+                                                                        .SetName("Name")
+                                                                        .SetTaskDate(_testDate)
+                                                                        .Execute();
+
+            // Verify
+            Assert.IsNotNull(result, "No task was created in the database");
+            Assert.AreEqual("Name", result.Name, "Task's name was incorrect");
+            Assert.AreEqual(_testDate, result.TaskDate, "Task's date value was incorrect");
+            Assert.IsFalse(result.Completed, "Task's completed status value is incorrect");
+            Assert.AreEqual(_contact.Id, result.Contact.Id, "Task's contact id value is incorrect");
+        }
+
+        [TestMethod]
+        public void Execute_Throws_Exception_When_Company_Not_Found()
+        {
+            // Setup
+            InitializeTestEntities();
+            int id = _contact.Id + 1;
+
+            // Act
+            try
+            {
+                new CreateTaskForContactCommand(_unitOfWork).WithContactId(id)
+                                                            .SetName("Name")
+                                                            .SetTaskDate(_testDate)
+                                                            .Execute();
+                Assert.Fail("Command did not throw an exception");
+            }
+
+            // Verify
+            catch (MJLEntityNotFoundException ex)
+            {
+                Assert.AreEqual(typeof(Contact), ex.EntityType, "MJLEntityNotFoundException's entity type was incorrect");
+                Assert.AreEqual(id.ToString(), ex.IdValue, "MJLEntityNotFoundException's id value was incorrect");
+            }
+        }
+    }
+}
