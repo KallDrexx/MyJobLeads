c8ee088d5f8b01439a35ad4a5d13948a66896ffd
mshapiro <kalldrexx@gmail.com>
2011-03-20 22:10:29 -0400

    Implemented castle windsor, with the controller installer and unitofwork installer

---
+++ MyJobLeads/App_Start/Bootstrapper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Web;
+using System.Web.Mvc;
+using Castle.Facilities.TypedFactory;
+using Castle.MicroKernel.Resolvers.SpecializedResolvers;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using MyJobLeads.App_Start;
+using MyJobLeads.Infrastructure;
+
+[assembly: WebActivator.PostApplicationStartMethod (
+    typeof(Bootstrapper), "Wire")]
+
+[assembly: WebActivator.ApplicationShutdownMethod (
+    typeof(Bootstrapper), "DeWire")]
+
+namespace MyJobLeads.App_Start
+{
+    public static class Bootstrapper
+    {
+        private static readonly IWindsorContainer container = new WindsorContainer();
+
+        public static void Wire()
+        {
+            //To be able to inject IEnumerable<T> ICollection<T> IList<T> T[] use this:
+            //container.Kernel.Resolver.AddSubResolver(new CollectionResolver(container.Kernel, true));
+            //Documentation http://docs.castleproject.org/Windsor.Resolvers.ashx
+            
+            //To support typed factories add this:
+            //container.AddFacility<TypedFactoryFacility>();
+            //Documentation http://docs.castleproject.org/Windsor.Typed-Factory-Facility.ashx
+            
+            container.Install(FromAssembly.This());
+			var controllerFactory = new WindsorControllerFactory(container.Kernel);
+			ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
+        public static void DeWire()
+        {
+            container.Dispose();
+        }
+    }
+}
---
+++ MyJobLeads/Infrastructure/Installers/UnitOfWorkInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using MyJobLeads.DomainModel.Data;
+
+namespace MyJobLeads.Infrastructure.Installers
+{
+    public class UnitOfWorkInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IUnitOfWork>().ImplementedBy<EFUnitOfWork>());
+        }
+    }
+}
