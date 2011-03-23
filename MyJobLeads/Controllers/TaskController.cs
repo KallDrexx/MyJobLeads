b327b339e7ee8d70621a52d71c000214ce845b37
mshapiro <kalldrexx@gmail.com>
2011-03-23 17:44:29 -0400

    Added the actions and views for the TaskController

---
+++ MyJobLeads/Controllers/TaskController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.Tasks;
+using MyJobLeads.DomainModel.Commands.Tasks;
+
+namespace MyJobLeads.Controllers
+{
+    public partial class TaskController : MyJobLeadsBaseController
+    {
+        public TaskController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public virtual ActionResult AddCompanyTask(int companyId)
+        {
+            ViewBag.CompanyId = companyId;
+            return View(MVC.Task.Views.Edit, new Task());
+        }
+
+        public virtual ActionResult AddContactTask(int contactId)
+        {
+            ViewBag.ContactId = contactId;
+            return View(MVC.Task.Views.Edit, new Task());
+        }
+
+        public virtual ActionResult Edit(int id)
+        {
+            var task = new TaskByIdQuery(_unitOfWork).WithTaskId(id).Execute();
+            return View(task);
+        }
+
+        [HttpPost]
+        public virtual ActionResult Edit(Task task, int companyId, int contactId)
+        {
+            // Determine if this is a new task or not
+            if (task.Id == 0)
+            {
+                // Determine if this task is for a company or contact
+                if (companyId != 0)
+                {
+                    task = new CreateTaskForCompanyCommand(_unitOfWork).WithCompanyId(companyId)
+                                                                       .SetName(task.Name)
+                                                                       .SetTaskDate(task.TaskDate)
+                                                                       .Execute();
+                }
+                else
+                {
+                    task = new CreateTaskForContactCommand(_unitOfWork).WithContactId(contactId)
+                                                                       .SetName(task.Name)
+                                                                       .SetTaskDate(task.TaskDate)
+                                                                       .Execute();
+                }
+            }
+            else
+            {
+                // Existing task
+                task = new EditTaskCommand(_unitOfWork).WithTaskId(task.Id)
+                                                       .SetName(task.Name)
+                                                       .SetTaskDate(task.TaskDate)
+                                                       .SetCompleted(task.Completed)
+                                                       .Execute();
+            }
+
+            return RedirectToAction(MVC.Task.View(task.Id));
+        }
+
+        public virtual ActionResult View(int id)
+        {
+            var task = new TaskByIdQuery(_unitOfWork).WithTaskId(id).Execute();
+            return View(task);
+        }
+    }
+}
