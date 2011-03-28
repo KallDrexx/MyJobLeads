87cedeffaa31baea6e9ccda6fdb3e0bfee8c0d54
mshapiro <kalldrexx@gmail.com>
2011-03-27 21:13:16 -0400

    Added the EditUserCommand class and tests

---
+++ MyJobLeads.DomainModel/Commands/Users/EditUserCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Exceptions;
+using MyJobLeads.DomainModel.Queries.Users;
+using MyJobLeads.DomainModel.Utilities;
+
+namespace MyJobLeads.DomainModel.Commands.Users
+{
+    /// <summary>
+    /// Command class that edits the properties of a user
+    /// </summary>
+    public class EditUserCommand
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected string _newEmail, _newPassword;
+        protected int _userId;
+
+        public EditUserCommand(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the user to edit
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public EditUserCommand WithUserId(int userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the email for the user
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public EditUserCommand SetEmail(string email)
+        {
+            _newEmail = email;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the new password for the user
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public EditUserCommand SetPassword(string password)
+        {
+            _newPassword = password;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="MJLEntityNotFoundException">Thrown when the specified user isn't found</exception>
+        public User Execute()
+        {
+            // Retrieve the user
+            var user = new UserByIdQuery(_unitOfWork).WithUserId(_userId).Execute();
+            if (user == null)
+                throw new MJLEntityNotFoundException(typeof(User), _userId);
+
+            // Edit the properties
+            if (_newEmail != null) { user.Email = _newEmail; }
+            if (_newPassword != null) { user.Password = PasswordUtils.CreatePasswordHash(user.Username, _newPassword); }
+
+            // Save
+            _unitOfWork.Commit();
+            return user;
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Commands/Users/EditUserCommandTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Utilities;
+using MyJobLeads.DomainModel.Commands.Users;
+using MyJobLeads.DomainModel.Exceptions;
+
+namespace MyJobLeads.Tests.Commands.Users
+{
+    [TestClass]
+    public class EditUserCommandTests : EFTestBase
+    {
+        private User _user;
+
+        private void InitializeTestEntities()
+        {
+            _user = new User
+            {
+                Username = "username",
+                Email = "starting@email.com",
+                Password = PasswordUtils.CreatePasswordHash("username", "starting password")
+            };
+
+            _unitOfWork.Users.Add(_user);
+            _unitOfWork.Commit();
+        }
+
+        [TestMethod]
+        public void Can_Edit_User_Entity()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            new EditUserCommand(_unitOfWork).WithUserId(_user.Id)
+                                            .SetEmail("new@email.com")
+                                            .SetPassword("new password")
+                                            .Execute();
+            User result = _unitOfWork.Users.Fetch().SingleOrDefault();
+
+            // Verify
+            Assert.IsNotNull(result, "No user was found in the repository");
+            Assert.AreEqual(_user, result, "User was incorrect");
+            Assert.AreEqual("new@email.com", result.Email, "User's email was incorrect");
+            Assert.IsTrue(PasswordUtils.CheckPasswordHash("username", "new password", result.Password), "User's password was incorrect");
+        }
+
+        [TestMethod]
+        public void Execute_Returns_Editted_User()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            User result = new EditUserCommand(_unitOfWork).WithUserId(_user.Id)
+                                                        .SetEmail("new@email.com")
+                                                        .SetPassword("new password")
+                                                        .Execute();
+             
+
+            // Verify
+            Assert.IsNotNull(result, "No user was found in the repository");
+            Assert.AreEqual(_user, result, "User was incorrect");
+            Assert.AreEqual("new@email.com", result.Email, "User's email was incorrect");
+            Assert.IsTrue(PasswordUtils.CheckPasswordHash("username", "new password", result.Password), "User's password was incorrect");
+        }
+
+        [TestMethod]
+        public void Execute_Throws_MJLEntityNotFoundException_When_User_Not_Found()
+        {
+            // Setup
+            InitializeTestEntities();
+            int id = _user.Id + 101;
+
+            // Act
+            try
+            {
+                new EditUserCommand(_unitOfWork).WithUserId(id)
+                                                .SetEmail("new@email.com")
+                                                .SetPassword("new password")
+                                                .Execute();
+                Assert.Fail("No exception was thrown");
+            }
+
+            // Verify
+            catch (MJLEntityNotFoundException ex)
+            {
+                Assert.AreEqual(typeof(User), ex.EntityType, "MJLEntityNotFoundException's entity type was incorrect");
+                Assert.AreEqual(id.ToString(), ex.IdValue, "MJLEntityNotFoundException's id value was incorrect");
+            }
+        }
+
+        [TestMethod]
+        public void Email_Is_Not_Changed_When_Not_Set()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            new EditUserCommand(_unitOfWork).WithUserId(_user.Id)
+                                            .SetPassword("new password")
+                                            .Execute();
+            User result = _unitOfWork.Users.Fetch().SingleOrDefault();
+
+            // Verify
+            Assert.IsNotNull(result, "No user was found in the repository");
+            Assert.AreEqual(_user, result, "User was incorrect");
+            Assert.AreEqual("starting@email.com", result.Email, "User's email was incorrect");
+            Assert.IsTrue(PasswordUtils.CheckPasswordHash("username", "new password", result.Password), "User's password was incorrect");
+        }
+
+        [TestMethod]
+        public void Password_Is_Not_Changed_When_Not_Specified()
+        {
+            // Setup
+            InitializeTestEntities();
+
+            // Act
+            new EditUserCommand(_unitOfWork).WithUserId(_user.Id)
+                                            .SetEmail("new@email.com")
+                                            .Execute();
+            User result = _unitOfWork.Users.Fetch().SingleOrDefault();
+
+            // Verify
+            Assert.IsNotNull(result, "No user was found in the repository");
+            Assert.AreEqual(_user, result, "User was incorrect");
+            Assert.AreEqual("new@email.com", result.Email, "User's email was incorrect");
+            Assert.IsTrue(PasswordUtils.CheckPasswordHash("username", "starting password", result.Password), "User's password was incorrect");
+        }
+    }
+}
