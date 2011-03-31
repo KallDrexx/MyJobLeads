5c8a1c04afa820f449e020bfb42c66d733098bc9
mshapiro <kalldrexx@gmail.com>
2011-03-30 23:29:09 -0400

    Removed default asp.net mvc ImembershipService comments

--- MyJobLeads/Models/Accounts/IMembershipService.cs
+++ MyJobLeads/Models/Accounts/IMembershipService.cs
@@ -6,11 +6,6 @@ using System.Web.Security;
 
 namespace MyJobLeads.Models.Accounts
 {
-    // The FormsAuthentication type is sealed and contains static members, so it is difficult to
-    // unit test code that calls its members. The interface and helper class below demonstrate
-    // how to create an abstract wrapper around such a type in order to make the AccountController
-    // code unit testable.
-
     public interface IMembershipService
     {
         int MinPasswordLength { get; }
