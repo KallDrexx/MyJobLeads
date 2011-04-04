f875cd7f907b9199ded7a0b0502f1283a3514c52
mshapiro <kalldrexx@gmail.com>
2011-04-03 23:10:30 -0400

    Extracted controller and builder functionality of the JobSearch controller tests into its own method

--- MyJobLeads.Tests/Controllers/JobSearchControllerTest.cs
+++ MyJobLeads.Tests/Controllers/JobSearchControllerTest.cs
@@ -14,6 +14,17 @@ namespace MyJobLeads.Tests.Controllers
     [TestClass]
     public class JobSearchControllerTest : InMemoryTestBase
     {
+        private JobSearchController _controller;
+        private TestControllerBuilder _builder;
+
+        [TestInitialize]
+        public void InitController()
+        {
+            _controller = new JobSearchController(_unitOfWork);
+            _builder = new TestControllerBuilder();
+            _builder.InitializeController(_controller);
+        }
+
         [TestMethod]
         public void Index_Shows_All_Job_Searches_For_User()
         {
@@ -26,20 +37,29 @@ namespace MyJobLeads.Tests.Controllers
             _unitOfWork.JobSearches.Add(search2);
             _unitOfWork.Commit();
 
-            JobSearchController controller = new JobSearchController(_unitOfWork);
-            controller.MembershipService = new MockMembershipService(user);
+            _controller.MembershipService = new MockMembershipService(user);
 
             // Act
-            ActionResult result = controller.Index();
+            ActionResult result = _controller.Index();
 
             // Verify
-            result.AssertViewRendered();
+            result.AssertViewRendered(); //.ForView("Index");
             IList<JobSearch> model = ((ViewResult)result).Model as List<JobSearch>;
 
             Assert.IsNotNull(model, "Action returned a null view model");
             Assert.AreEqual(2, model.Count, "Returned view model had an incorrect number of elements");
             Assert.AreEqual(search1.Id, model[0].Id, "First job search had an incorrect id value");
             Assert.AreEqual(search2.Id, model[1].Id, "Second job search had an incorrect id value");
+        }
+
+        [TestMethod]
+        public void Add_Returns_Edit_View()
+        {
+            // Act
+            ActionResult result = _controller.Add();
+
+            // Verify
+            result.AssertViewRendered(); //.ForView("Edit");
         }
     }
 }
