6ba5bea80c644c7f3e9c1493876dfa1a418002b4
unknown <KallDrexx@.(none)>
2011-05-02 19:01:33 -0400

    Added company and contact information to the sidebar when editing tasks

--- MyJobLeads/ViewModels/Tasks/EditTaskViewModel.cs
+++ MyJobLeads/ViewModels/Tasks/EditTaskViewModel.cs
@@ -13,6 +13,7 @@ namespace MyJobLeads.ViewModels.Tasks
 
         public EditTaskViewModel(Company company) 
         {
+            Company = company;
             AssociatedCompanyId = company.Id;
         }
 
@@ -21,18 +22,22 @@ namespace MyJobLeads.ViewModels.Tasks
             Id = task.Id;
             Name = task.Name;
             TaskDate = task.TaskDate;
-            AssociatedCompanyId = Convert.ToInt32(task.CompanyId);
             Completed = task.Completed;
             AssociatedContactId = Convert.ToInt32(task.ContactId);
+            Company = task.Company;
+            Contact = task.Contact;
+            AssociatedContactId = task.ContactId ?? 0;
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime? TaskDate { get; set; }
         public bool Completed { get; set; }
+        public Company Company { get; set; }
+        public Contact Contact { get; set; }
 
-        public int AssociatedCompanyId { get; set; }
         public int AssociatedContactId { get; set; }
+        public int AssociatedCompanyId { get; set; }
 
         public IList<SelectListItem> CompanyContactList { get; set; }
     }
