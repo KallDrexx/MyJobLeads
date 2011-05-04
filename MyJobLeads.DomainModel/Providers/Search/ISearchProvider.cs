5a29193ce1d78eb5120a0c13cb21d32d2203ff3e
unknown <KallDrexx@.(none)>
2011-05-04 00:19:56 -0400

    Added the ISearchProvider interface

---
+++ MyJobLeads.DomainModel/Providers/Search/ISearchProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.ViewModels;
+
+namespace MyJobLeads.DomainModel.Providers.Search
+{
+    public interface ISearchProvider
+    {
+        void Add(Company company);
+        void Add(Contact contact);
+        void Add(Task task);
+
+        void Remove(Company company);
+        void Remove(Contact contact);
+        void Remove(Task task);
+
+        SearchProviderResult Search(string searchString);
+    }
+}
---
+++ MyJobLeads.DomainModel/ViewModels/SearchProviderResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyJobLeads.DomainModel.ViewModels
+{
+    public class SearchProviderResult
+    {
+        public SearchProviderResult()
+        {
+            FoundCompanyIds = new List<int>();
+            FoundContactIds = new List<int>();
+            FoundTaskIds = new List<int>();
+        }
+
+        public IList<int> FoundCompanyIds { get; protected set; }
+        public IList<int> FoundContactIds { get; protected set; }
+        public IList<int> FoundTaskIds { get; protected set; }
+    }
+}
