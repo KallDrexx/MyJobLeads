af08fb6b624a8d6ad7e8c5b420c48b1abd27499e
unknown <KallDrexx@.(none)>
2011-05-07 13:26:49 -0400

    Added functionality to index and remove companies from the LuceneSearchProvider

---
+++ MyJobLeads.DomainModel/Providers/Search/LuceneSearchProvider.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.ViewModels;
+using MyJobLeads.DomainModel.Entities;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using System.IO;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Analysis;
+using Lucene.Net.Documents;
+using Lucene.Net.Search;
+
+namespace MyJobLeads.DomainModel.Providers.Search
+{
+    public class LuceneSearchProvider : ISearchProvider
+    {
+        public LuceneSearchProvider()
+        {
+            LuceneIndexBaseDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\MyJobLeadsIndex";
+        }
+
+        #region Properties
+
+        public string LuceneIndexBaseDirectory { get; protected set; }
+
+        #endregion
+
+        #region Indexing Methods
+
+        public void Index(Company company)
+        {
+            if (company == null)
+                throw new ArgumentNullException("Cannot index a null company");
+
+            // Create a document for the company
+            var document = new Document();
+            document.Add(new Field(Constants.COMPANY_ID, company.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            document.Add(new Field(Constants.COMPANY_NAME, company.Name ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field(Constants.COMPANY_CITY, company.City ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field(Constants.COMPANY_INDUSTRY, company.Industry ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field(Constants.COMPANY_METRO, company.MetroArea ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field(Constants.COMPANY_NOTES, company.Notes ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field(Constants.COMPANY_PHONE, company.Phone ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field(Constants.COMPANY_STATE, company.State ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field(Constants.COMPANY_ZIP, company.Zip ?? string.Empty, Field.Store.YES, Field.Index.ANALYZED));
+
+            // Remove any previous documents for the company and add the new one
+            Remove(company);
+
+            var writer = OpenIndex();
+            try
+            {
+                writer.AddDocument(document);
+                writer.Optimize();
+            }
+            finally 
+            {
+                // Make sure the writer attempts to close even if an exception occurs, to prevent stale locks
+                writer.Close(); 
+            }
+        }
+
+        public void Index(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Index(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Company company)
+        {
+            if (company == null)
+                throw new ArgumentNullException("Cannot remove the index for a null company");
+
+            // Delete all documents that have a company id matching the passed in company
+            var writer = OpenIndex();
+            writer.DeleteDocuments(new Term(Constants.COMPANY_ID, company.Id.ToString()));
+            writer.Optimize();
+            writer.Close();
+        }
+
+        public void Remove(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Search Methods
+
+        public SearchProviderResult Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Utility Methods
+
+        /// <summary>
+        /// Creates an index writer for 
+        /// </summary>
+        /// <returns></returns>
+        public IndexWriter OpenIndex()
+        {
+            var directory = FSDirectory.Open(new DirectoryInfo(LuceneIndexBaseDirectory));
+
+            // Open the index
+            bool createNewIndex = !IndexReader.IndexExists(directory);
+            var writer = new IndexWriter(directory, CreateAnalyzer(), createNewIndex, IndexWriter.MaxFieldLength.UNLIMITED);
+
+            return writer;
+        }
+
+        /// <summary>
+        /// Creates an analyzer for usage by index reader and writers
+        /// </summary>
+        /// <returns></returns>
+        public Analyzer CreateAnalyzer()
+        {
+            return new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_29);
+        }
+
+        #endregion
+
+        #region Constants
+
+        public static class Constants
+        {
+            // Document field name constants for all searchable fields
+            public const string COMPANY_ID = "company_id";
+            public const string COMPANY_NAME = "company_name";
+            public const string COMPANY_PHONE = "company_phone";
+            public const string COMPANY_CITY = "company_city";
+            public const string COMPANY_STATE = "company_state";
+            public const string COMPANY_ZIP = "company_zip";
+            public const string COMPANY_METRO = "company_metroarea";
+            public const string COMPANY_INDUSTRY = "company_industry";
+            public const string COMPANY_NOTES = "company_notes";
+
+            public const string CONTACT_ID = "contact_id";
+            public const string CONTACT_NAME = "contact_name";
+            public const string CONTACT_DIRECTPHONE = "contact_directphone";
+            public const string CONTACT_MOBILEPHONE = "contact_mobilephone";
+            public const string CONTACT_EXTENSION = "contact_extension";
+            public const string CONTACT_EMAIL = "contact_email";
+            public const string CONTACT_ASSISTANT = "contact_assistant";
+            public const string CONTACT_REFERREDBY = "contact_referredby";
+            public const string CONTACT_NOTES = "contact_notes";
+
+            public const string TASK_ID = "task_id";
+            public const string TASK_NAME = "task_name";
+        }
+
+        #endregion
+    }
+}
---
+++ MyJobLeads.Tests/Providers/LuceneSearchProviderTests.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Providers.Search;
+using System.IO;
+using Lucene.Net.Index;
+using Lucene.Net.Analysis.Standard;
+using MyJobLeads.DomainModel.Entities;
+using Lucene.Net.Store;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Documents;
+
+namespace MyJobLeads.Tests.Providers
+{
+    [TestClass]
+    public class LuceneSearchProviderTests
+    {
+        private LuceneSearchProvider _provider;
+        private FSDirectory _indexDirectory;
+
+        private TopDocs SearchIndex(string searchField, string searchQuery, out IndexSearcher searcher)
+        {
+            var parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_29, searchField, new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_29));
+            searcher = new IndexSearcher(_indexDirectory, true);
+            return searcher.Search(parser.Parse(searchQuery), 2);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _provider = new LuceneSearchProvider();
+
+            // Load the index and delete all documents so it's fresh for the test
+            _indexDirectory = Lucene.Net.Store.FSDirectory.Open(new DirectoryInfo(_provider.LuceneIndexBaseDirectory));
+            var writer = new IndexWriter(_indexDirectory, new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_29), true, IndexWriter.MaxFieldLength.UNLIMITED);
+            writer.DeleteAll();
+            writer.Close();
+        }
+
+        [TestMethod]
+        public void Can_Index_Company()
+        {
+            // Setup
+            Company company = new Company
+            {
+                Id = 3,
+                City = "City",
+                Industry = "Industry",
+                MetroArea = "Metro",
+                Name = "Name",
+                Notes = "Notes",
+                Phone = "Phone",
+                State = "State",
+                Zip = "Zip"
+            };
+
+            // Act
+            _provider.Index(company);
+
+            // Verify
+            IndexSearcher searcher;
+            TopDocs hits = SearchIndex(LuceneSearchProvider.Constants.COMPANY_ID, "3", out searcher);
+            Assert.AreEqual(1, hits.scoreDocs.Length, "Incorrect number of documents returned");
+
+            Document doc = searcher.Doc(hits.scoreDocs[0].doc);
+            Assert.AreEqual("3", doc.Get(LuceneSearchProvider.Constants.COMPANY_ID), "Document had an incorrect company id value");
+            Assert.AreEqual("City", doc.Get(LuceneSearchProvider.Constants.COMPANY_CITY), "Document had an incorrect company city value");
+            Assert.AreEqual("Industry", doc.Get(LuceneSearchProvider.Constants.COMPANY_INDUSTRY), "Document had an incorrect company industry value");
+            Assert.AreEqual("Metro", doc.Get(LuceneSearchProvider.Constants.COMPANY_METRO), "Document had an incorrect metro area value ");
+            Assert.AreEqual("Name", doc.Get(LuceneSearchProvider.Constants.COMPANY_NAME), "Document had an incorrect name value");
+            Assert.AreEqual("Notes", doc.Get(LuceneSearchProvider.Constants.COMPANY_NOTES), "Document had an incorrect notes value");
+            Assert.AreEqual("Phone", doc.Get(LuceneSearchProvider.Constants.COMPANY_PHONE), "Document had an incorrect phone value");
+            Assert.AreEqual("State", doc.Get(LuceneSearchProvider.Constants.COMPANY_STATE), "Document had an incorrect state value");
+            Assert.AreEqual("Zip", doc.Get(LuceneSearchProvider.Constants.COMPANY_ZIP), "Document had an incorrect zip value");
+        }
+
+        [TestMethod]
+        public void Indexed_Companys_Null_Values_Become_Empty_Strings()
+        {
+            // Setup
+            Company company = new Company { Id = 3 };
+
+            // Act
+            _provider.Index(company);
+
+            // Verify
+            IndexSearcher searcher;
+            TopDocs hits = SearchIndex(LuceneSearchProvider.Constants.COMPANY_ID, "3", out searcher);
+            Assert.AreEqual(1, hits.scoreDocs.Length, "Incorrect number of documents returned");
+
+            Document doc = searcher.Doc(hits.scoreDocs[0].doc);
+            Assert.AreEqual("3", doc.Get(LuceneSearchProvider.Constants.COMPANY_ID), "Document had an incorrect company id value");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_CITY), "Document had an incorrect company city value");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_INDUSTRY), "Document had an incorrect company industry value");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_METRO), "Document had an incorrect metro area value ");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_NAME), "Document had an incorrect name value");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_NOTES), "Document had an incorrect notes value");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_PHONE), "Document had an incorrect phone value");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_STATE), "Document had an incorrect state value");
+            Assert.AreEqual(string.Empty, doc.Get(LuceneSearchProvider.Constants.COMPANY_ZIP), "Document had an incorrect zip value");
+        }
+
+        [TestMethod]
+        public void Can_Index_Multiple_Companies()
+        {
+            // Setup
+            var company1 = new Company { Id = 3, Name = "Name" };
+            var company2 = new Company { Id = 4, Name = "Name" };
+
+            // Act
+            _provider.Index(company1);
+            _provider.Index(company2);
+
+            // Verify
+            IndexSearcher searcher;
+            TopDocs hits = SearchIndex(LuceneSearchProvider.Constants.COMPANY_NAME, "Name", out searcher);
+
+            Assert.AreEqual(2, hits.scoreDocs.Length, "Incorrect number of documents returned");
+        }
+
+        [TestMethod]
+        public void ReIndexing_Company_Updates_Existing_Company_Document()
+        {
+            // Setup
+            Company company = new Company { Id = 3, Name = "Name1" };
+            _provider.Index(company);
+            company.Name = "Name2";
+
+            // Act
+            _provider.Index(company);
+
+            // Verify
+            IndexSearcher searcher;
+            TopDocs hits = SearchIndex(LuceneSearchProvider.Constants.COMPANY_ID, "3", out searcher);
+            Assert.AreEqual(1, hits.scoreDocs.Length, "Incorrect number of documents returned");
+
+            Document doc = searcher.Doc(hits.scoreDocs[0].doc);
+            Assert.AreEqual("Name2", doc.Get(LuceneSearchProvider.Constants.COMPANY_NAME), "Document had an incorrect name");
+        }
+
+        [TestMethod]
+        public void Can_Remove_Company_From_Index()
+        {
+            // Setup
+            Company company = new Company { Id = 3 };
+            _provider.Index(company);
+
+            // Act
+            _provider.Remove(company);
+
+            // Verify
+            IndexSearcher searcher;
+            TopDocs hits = SearchIndex(LuceneSearchProvider.Constants.COMPANY_ID, "3", out searcher);
+
+            Assert.AreEqual(0, hits.scoreDocs.Length, "Incorrect number of documents returned");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Indexing_Null_Company_Throws_ArgumentNullException()
+        {
+            // Act
+            _provider.Index((Company)null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Removing_Null_Company_Throws_ArgumentNullException()
+        {
+            // Act
+            _provider.Remove((Company)null);
+        }
+    }
+}
