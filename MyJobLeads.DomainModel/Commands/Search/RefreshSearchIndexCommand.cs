fa167e4dee7a6e17cfb9fe7b62de58bf0acd61c9
unknown <KallDrexx@.(none)>
2011-05-08 11:47:23 -0400

    Added RefreshSearchIndex command and tests to index all entities in the database

---
+++ MyJobLeads.DomainModel/Commands/Search/RefreshSearchIndexCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Providers.Search;
+using MyJobLeads.DomainModel.Entities;
+
+namespace MyJobLeads.DomainModel.Commands.Search
+{
+    public class RefreshSearchIndexCommand
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected ISearchProvider _searchProvider;
+
+        public RefreshSearchIndexCommand(IUnitOfWork unitOfWork, ISearchProvider searchProvider)
+        {
+            _unitOfWork = unitOfWork;
+            _searchProvider = searchProvider;
+        }
+
+        /// <summary>
+        /// Executes the command
+        /// </summary>
+        public void Execute()
+        {
+            // Retrieve all companies from the database, index them, then release them from memory
+            var companies = _unitOfWork.Companies.Fetch().ToList();
+            foreach (var company in companies)
+                _searchProvider.Index(company);
+
+            companies = null;
+            GC.Collect();
+
+            // Retrieve all contacts from the database, index them, then release them from memory
+            var contacts = _unitOfWork.Contacts.Fetch().ToList();
+            foreach (var contact in contacts)
+                _searchProvider.Index(contact);
+
+            contacts = null;
+            GC.Collect();
+
+            // Retrieve all tasks from the database, index them, then release them from memory
+            var tasks = _unitOfWork.Tasks.Fetch().ToList();
+            foreach (var task in tasks)
+                _searchProvider.Index(task);
+
+            tasks = null;
+            GC.Collect();
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Commands/Search/RefreshSearchIndexCommandTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Commands.Search;
+using Moq;
+using MyJobLeads.DomainModel.Providers.Search;
+
+namespace MyJobLeads.Tests.Commands.Search
+{
+    [TestClass]
+    public class RefreshSearchIndexCommandTests : EFTestBase
+    {
+        [TestMethod]
+        public void Command_Updates_Index_For_All_Entities_In_Database()
+        {
+            // Setup
+            var mock = new Mock<ISearchProvider>();
+
+            JobSearch search = new JobSearch();
+            Company company1 = new Company { JobSearch = search }, company2 = new Company { JobSearch = search };
+            Contact contact = new Contact { Company = company1 };
+            Task task = new Task { Company = company2 };
+
+            _unitOfWork.JobSearches.Add(search);
+            _unitOfWork.Companies.Add(company1);
+            _unitOfWork.Companies.Add(company2);
+            _unitOfWork.Contacts.Add(contact);
+            _unitOfWork.Tasks.Add(task);
+            _unitOfWork.Commit();
+
+            // Act
+            new RefreshSearchIndexCommand(_unitOfWork, mock.Object).Execute();
+
+            // Verify
+            mock.Verify(x => x.Index(company1), Times.Once());
+            mock.Verify(x => x.Index(company2), Times.Once());
+            mock.Verify(x => x.Index(contact), Times.Once());
+            mock.Verify(x => x.Index(task), Times.Once());
+        }
+    }
+}
