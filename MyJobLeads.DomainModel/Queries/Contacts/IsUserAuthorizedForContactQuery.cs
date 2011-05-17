cb13b170ec28869f811b2b0589ca0c5b623ede08
unknown <KallDrexx@.(none)>
2011-05-17 17:51:35 -0400

    Added IsUserAuthorizedForContactQuery class and tests

---
+++ MyJobLeads.DomainModel/Queries/Contacts/IsUserAuthorizedForContactQuery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+
+namespace MyJobLeads.DomainModel.Queries.Contacts
+{
+    /// <summary>
+    /// Query that determines if the specified user is authorized to access the specified contact
+    /// </summary>
+    public class IsUserAuthorizedForContactQuery
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected int _userId, _contactId;
+
+        public IsUserAuthorizedForContactQuery(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the user to check authorization for
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForContactQuery WithUserId(int userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the contact to check authorization for
+        /// </summary>
+        /// <param name="contactId"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForContactQuery WithContactId(int contactId)
+        {
+            _contactId = contactId;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the query
+        /// </summary>
+        /// <returns></returns>
+        public bool Execute()
+        {
+            // Determine if the specified user created the jobsearch the contact is associated with
+            return _unitOfWork.Contacts.Fetch().Any(x => x.Id == _contactId && x.JobSearch.User.Id == _userId);
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Queries/Contacts/IsUserAuthorizedForContactQueryTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.Contacts;
+
+namespace MyJobLeads.Tests.Queries.Contacts
+{
+    [TestClass]
+    public class IsUserAuthorizedForContactQueryTests : EFTestBase
+    {
+        [TestMethod]
+        public void User_Associated_With_Contacts_JobSearch_Is_Authorized()
+        {
+            // Setup
+            User user = new User();
+            JobSearch jobSearch = new JobSearch { User = user };
+            Contact contact = new Contact { JobSearch = jobSearch };
+
+            _unitOfWork.Contacts.Add(contact);
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.JobSearches.Add(jobSearch);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForContactQuery(_unitOfWork).WithUserId(user.Id).WithContactId(contact.Id).Execute();
+
+            // Verify
+            Assert.IsTrue(result, "User was incorrectly not authorized for the specified contact");
+        }
+
+        [TestMethod]
+        public void User_Not_Associated_With_Contacts_JobSearch_Is_Not_Authorized()
+        {
+            // Setup
+            User user = new User(), user2 = new User();
+            JobSearch jobSearch = new JobSearch { User = user };
+            Contact contact = new Contact { JobSearch = jobSearch };
+
+            _unitOfWork.Contacts.Add(contact);
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.Users.Add(user2);
+            _unitOfWork.JobSearches.Add(jobSearch);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForContactQuery(_unitOfWork).WithUserId(user2.Id).WithContactId(contact.Id).Execute();
+
+            // Verify
+            Assert.IsFalse(result, "User was incorrectly not authorized for the specified contact");
+        }
+    }
+}
