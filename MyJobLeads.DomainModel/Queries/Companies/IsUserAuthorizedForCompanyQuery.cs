e51022e9d8e920aa204a4526db79f858962d08c7
unknown <KallDrexx@.(none)>
2011-05-17 00:26:14 -0400

    Created the IsUserAuthorizedForCompanyQuery class and tests

---
+++ MyJobLeads.DomainModel/Queries/Companies/IsUserAuthorizedForCompanyQuery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Data;
+
+namespace MyJobLeads.DomainModel.Queries.Companies
+{
+    /// <summary>
+    /// Query that determines if the specified user is authorized for the specified company
+    /// </summary>
+    public class IsUserAuthorizedForCompanyQuery
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected int _userId, _companyId;
+
+        public IsUserAuthorizedForCompanyQuery(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the user to check authorization for
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForCompanyQuery WithUserId(int userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the company to check a user's authorization for
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForCompanyQuery WithCompanyId(int companyId)
+        {
+            _companyId = companyId;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the query
+        /// </summary>
+        /// <returns>Returns true if the user is authorized for access to the specified company, false if they are not</returns>
+        public bool Execute()
+        {
+            return _unitOfWork.Companies.Fetch().Count(x => x.Id == _companyId && x.CreatedByUserId == _userId) > 0;
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Commands/Companies/IsUserAuthorizedForCompanyQueryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.Companies;
+
+namespace MyJobLeads.Tests.Commands.Companies
+{
+    [TestClass]
+    public class IsUserAuthorizedForCompanyQueryTests : EFTestBase
+    {
+        [TestMethod]
+        public void User_That_Created_Company_Is_Authorized()
+        {
+            // Setup
+            User user = new User();
+            Company company = new Company { CreatedByUser = user };
+
+            _unitOfWork.Companies.Add(company);
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForCompanyQuery(_unitOfWork).WithUserId(user.Id).WithCompanyId(company.Id).Execute();
+
+            // Verify
+            Assert.IsTrue(result, "User was incorrectly not authorized for the specified company");
+        }
+
+        [TestMethod]
+        public void User_That_Didnt_Create_Company_Is_Not_Authorized()
+        {
+            // Setup
+            User user = new User(), user2 = new User();
+            Company company = new Company { CreatedByUser = user };
+
+            _unitOfWork.Companies.Add(company);
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.Users.Add(user2);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForCompanyQuery(_unitOfWork).WithUserId(user2.Id).WithCompanyId(company.Id).Execute();
+
+            // Verify
+            Assert.IsFalse(result, "User was incorrectly not authorized for the specified company");
+        }
+    }
+}
