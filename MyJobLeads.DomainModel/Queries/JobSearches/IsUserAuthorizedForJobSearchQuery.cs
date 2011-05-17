0bf097b08230b8c3d6a49f0cd9ec88992567d2a1
unknown <KallDrexx@.(none)>
2011-05-17 18:52:39 -0400

    Created the IsUserAuthorizedForJobSearchQuery class and tests

---
+++ MyJobLeads.DomainModel/Queries/JobSearches/IsUserAuthorizedForJobSearchQuery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+
+namespace MyJobLeads.DomainModel.Queries.JobSearches
+{
+    /// <summary>
+    /// Query that determines if a user has access to a jobsearch
+    /// </summary>
+    public class IsUserAuthorizedForJobSearchQuery
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected int _userId, _searchId;
+
+        public IsUserAuthorizedForJobSearchQuery(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies id value of the user to check authorization for
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForJobSearchQuery WithUserId(int userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the job search to check authorization for
+        /// </summary>
+        /// <param name="jobSearchid"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForJobSearchQuery WithJobSearchId(int jobSearchid)
+        {
+            _searchId = jobSearchid;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the query
+        /// </summary>
+        /// <returns>Returns true if the user is authorized to access the job search</returns>
+        public bool Execute()
+        {
+            // Check if the user is associated with the job search
+            return _unitOfWork.JobSearches.Fetch().Any(x => x.Id == _searchId && x.User.Id == _userId);
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Queries/JobSearches/IsUserAuthorizedForJobSearchQueryTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.JobSearches;
+
+namespace MyJobLeads.Tests.Queries.JobSearches
+{
+    [TestClass]
+    public class IsUserAuthorizedForJobSearchQueryTests : EFTestBase
+    {
+        [TestMethod]
+        public void User_Associated_With_Contacts_JobSearch_Is_Authorized()
+        {
+            // Setup
+            User user = new User();
+            JobSearch jobSearch = new JobSearch { User = user };
+
+            _unitOfWork.JobSearches.Add(jobSearch);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForJobSearchQuery(_unitOfWork).WithUserId(user.Id).WithJobSearchId(jobSearch.Id).Execute();
+
+            // Verify
+            Assert.IsTrue(result, "User was incorrectly not authorized for the job search");
+        }
+
+        [TestMethod]
+        public void User_Not_Associated_With_Contacts_JobSearch_Is_Not_Authorized()
+        {
+            // Setup
+            User user = new User(), user2 = new User();
+            JobSearch jobSearch = new JobSearch { User = user };
+            Contact contact = new Contact { JobSearch = jobSearch };
+
+            _unitOfWork.Contacts.Add(contact);
+            _unitOfWork.Users.Add(user);
+            _unitOfWork.Users.Add(user2);
+            _unitOfWork.JobSearches.Add(jobSearch);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForJobSearchQuery(_unitOfWork).WithUserId(user2.Id).WithJobSearchId(jobSearch.Id).Execute();
+
+            // Verify
+            Assert.IsFalse(result, "User was incorrectly authorized for the job search");
+        }
+    }
+}
