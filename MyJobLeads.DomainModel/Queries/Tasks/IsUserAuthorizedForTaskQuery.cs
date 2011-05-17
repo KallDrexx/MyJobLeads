271a3415c8fffc5bd08922939881389feff75d34
unknown <KallDrexx@.(none)>
2011-05-17 18:40:03 -0400

    Created the IsUserAuthorizedForTaskQuery class and tests

---
+++ MyJobLeads.DomainModel/Queries/Tasks/IsUserAuthorizedForTaskQuery.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+
+namespace MyJobLeads.DomainModel.Queries.Tasks
+{
+    /// <summary>
+    /// Query that determines if a user is authorized for access to a task
+    /// </summary>
+    public class IsUserAuthorizedForTaskQuery
+    {
+        protected IUnitOfWork _unitOfWork;
+        protected int _userId, _taskId;
+
+        public IsUserAuthorizedForTaskQuery(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the user to check authorization for
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForTaskQuery WithUserId(int userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        /// <summary>
+        /// Specifies the id value of the task to check authorization for
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <returns></returns>
+        public IsUserAuthorizedForTaskQuery WithTaskId(int taskId)
+        {
+            _taskId = taskId;
+            return this;
+        }
+
+        /// <summary>
+        /// Executes the result
+        /// </summary>
+        /// <returns>Returns true if the specified user is authorized for access to the task</returns>
+        public bool Execute()
+        {
+            // Check if the task's associated job search belongs to the user
+            return _unitOfWork.Tasks.Fetch().Any(x => x.Id == _taskId && x.Company.JobSearch.User.Id == _userId);
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Queries/Tasks/IsUserAuthorizedForTaskQueryTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Queries.Tasks;
+
+namespace MyJobLeads.Tests.Queries.Tasks
+{
+    [TestClass]
+    public class IsUserAuthorizedForTaskQueryTests : EFTestBase
+    {
+        [TestMethod]
+        public void User_Associated_With_Tasks_JobSearch_Is_Authorized()
+        {
+            // Setup
+            User user = new User();
+            JobSearch jobSearch = new JobSearch { User = user };
+            Company company = new Company { JobSearch = jobSearch };
+            Task task = new Task { Company = company };
+
+            _unitOfWork.Tasks.Add(task);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForTaskQuery(_unitOfWork).WithUserId(user.Id).WithTaskId(task.Id).Execute();
+
+            // Verify
+            Assert.IsTrue(result, "User was incorrectly not authorized for the task");
+        }
+
+        [TestMethod]
+        public void User_Not_Associated_With_Tasks_JobSearch_Is_Not_Authorized()
+        {
+            // Setup
+            User user = new User(), user2 = new User();
+            JobSearch jobSearch = new JobSearch { User = user };
+            Company company = new Company { JobSearch = jobSearch };
+            Task task = new Task { Company = company };
+
+            _unitOfWork.Tasks.Add(task);
+            _unitOfWork.Users.Add(user2);
+            _unitOfWork.Commit();
+
+            // Act
+            bool result = new IsUserAuthorizedForTaskQuery(_unitOfWork).WithUserId(user2.Id).WithTaskId(task.Id).Execute();
+
+            // Verify
+            Assert.IsFalse(result, "User was incorrectly authorized for the contact");
+        }
+    }
+}
