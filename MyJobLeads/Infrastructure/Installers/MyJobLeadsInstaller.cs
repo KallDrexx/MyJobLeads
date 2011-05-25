fd32f9f059346d1faee52c6d8e37d013b89cc554
unknown <KallDrexx@.(none)>
2011-05-24 20:26:35 -0400

    Added code to the MJL installer to help lessen the "The context cannot be used while the model is being created" errors

--- MyJobLeads/Infrastructure/Installers/MyJobLeadsInstaller.cs
+++ MyJobLeads/Infrastructure/Installers/MyJobLeadsInstaller.cs
@@ -8,6 +8,8 @@ using Castle.Windsor;
 using MyJobLeads.DomainModel.Data;
 using MyJobLeads.DomainModel.Providers.Search;
 using MyJobLeads.DomainModel.Providers.DataDirectory;
+using MyJobLeads.DomainModel.Entities.EF;
+using MyJobLeads.DomainModel.Entities;
 
 namespace MyJobLeads.Infrastructure.Installers
 {
@@ -15,9 +17,16 @@ namespace MyJobLeads.Infrastructure.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            container.Register(Component.For<IUnitOfWork>().ImplementedBy<EFUnitOfWork>().LifeStyle.PerWebRequest);
             container.Register(Component.For<ISearchProvider>().ImplementedBy<LuceneSearchProvider>().LifeStyle.PerWebRequest);
             container.Register(Component.For<IDataDirectoryProvider>().ImplementedBy<AppDataDirectoryProvider>().LifeStyle.Singleton);
+
+            // After registering the EFUnitOfWork, we have to create a new MyJobLeadsDbContext, or else the
+            //   errors will occur if the site is starting up and a 2nd request is made
+            container.Register(Component.For<IUnitOfWork>().ImplementedBy<EFUnitOfWork>().LifeStyle.PerWebRequest);
+            using (var context = new MyJobLeadsDbContext())
+            {
+                context.Set<UnitTestEntity>().Any();
+            }
         }
     }
 }
