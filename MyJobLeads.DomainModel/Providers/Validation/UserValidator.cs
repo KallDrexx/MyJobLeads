dd452aef80b551391ff6e061050f71503738a3c7
mshapiro <kalldrexx@gmail.com>
2011-07-15 21:55:42 -0400

    Created the UserValidator and tests

---
+++ MyJobLeads.DomainModel/Providers/Validation/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Entities;
+using FluentValidation;
+
+namespace MyJobLeads.DomainModel.Providers.Validation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Entered email is not a valid email address");
+        }
+    }
+}
