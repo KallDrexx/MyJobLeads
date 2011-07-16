8ee39864544d04fdd0bb2a2f71e023ca89a5ad60
mshapiro <kalldrexx@gmail.com>
2011-07-15 21:44:52 -0400

    Added the TaskValidator and tests

---
+++ MyJobLeads.DomainModel/Providers/Validation/TaskValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Entities;
+using FluentValidation;
+
+namespace MyJobLeads.DomainModel.Providers.Validation
+{
+    public class TaskValidator : AbstractValidator<Task>
+    {
+        public TaskValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("The task must have a subject");
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Providers/Validators/TaskValidatorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Providers.Validation;
+
+namespace MyJobLeads.Tests.Providers.Validators
+{
+    [TestClass]
+    public class TaskValidatorTests
+    {
+        private Task _task;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _task = new Task
+            {
+                Name = "Name"
+            };
+        }
+
+        [TestMethod]
+        public void Correct_Task_Passes_Validation()
+        {
+            // Act
+            bool result = new TaskValidator().Validate(_task).IsValid;
+
+            // Verify
+            Assert.IsTrue(result, "Task was incorrectly marked as invalid");
+        }
+
+        [TestMethod]
+        public void Task_With_Empty_Name_Is_Invalid()
+        {
+            // Setup
+            _task.Name = " ";
+
+            // Act
+            bool result = new TaskValidator().Validate(_task).IsValid;
+
+            // Verify
+            Assert.IsFalse(result, "Task was incorrectly marked as valid");
+        }
+
+        [TestMethod]
+        public void Task_With_Null_Name_Is_Invalid()
+        {
+            // Setup
+            _task.Name = null;
+
+            // Act
+            bool result = new TaskValidator().Validate(_task).IsValid;
+
+            // Verify
+            Assert.IsFalse(result, "Task was incorrectly marked as valid");
+        }
+    }
+}
