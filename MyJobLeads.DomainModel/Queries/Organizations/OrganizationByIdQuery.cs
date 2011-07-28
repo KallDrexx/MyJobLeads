060ef0dcdca4070812c0887d0903923bb535e4e7
mshapiro <kalldrexx@gmail.com>
2011-07-27 22:17:01 -0400

    Added the OrganizationByIdQuery and tests

---
+++ MyJobLeads.DomainModel/Queries/Organizations/OrganizationByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.Providers;
+
+namespace MyJobLeads.DomainModel.Queries.Organizations
+{
+    public struct OrganizationByIdQueryParams
+    {
+        public int OrganizationId { get; set; }
+    }
+
+    public class OrganizationByIdQuery
+    {
+        protected IServiceFactory _serviceFactory;
+
+        public OrganizationByIdQuery(IServiceFactory iServiceFactory)
+        {
+            _serviceFactory = iServiceFactory;
+        }
+
+        public Organization Execute(OrganizationByIdQueryParams queryParams)
+        {
+            var unitOfWork = _serviceFactory.GetService<IUnitOfWork>();
+
+            return unitOfWork.Organizations.Fetch().Where(x => x.Id == queryParams.OrganizationId).SingleOrDefault();
+        }
+    }
+}
