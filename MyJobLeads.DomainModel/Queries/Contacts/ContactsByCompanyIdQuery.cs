aa56133121cf200c55ab6ef7adf50659c783510e
mshapiro <kalldrexx@gmail.com>
2011-10-02 18:25:21 -0400

    Added authorization to the ContactsByCompany query

--- MyJobLeads.DomainModel/Queries/Contacts/ContactsByCompanyIdQuery.cs
+++ MyJobLeads.DomainModel/Queries/Contacts/ContactsByCompanyIdQuery.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using MyJobLeads.DomainModel.Data;
 using MyJobLeads.DomainModel.Entities;
+using MyJobLeads.DomainModel.ProcessParams.Security;
+using MyJobLeads.DomainModel.ViewModels.Authorizations;
 
 namespace MyJobLeads.DomainModel.Queries.Contacts
 {
@@ -13,11 +15,13 @@ namespace MyJobLeads.DomainModel.Queries.Contacts
     public class ContactsByCompanyIdQuery
     {
         protected IUnitOfWork _unitOfWork;
-        protected int _companyId;
+        protected IProcess<CompanyQueryAuthorizationParams, AuthorizationResultViewModel> _compAuthProcess;
+        protected int _companyId, _userId;
 
-        public ContactsByCompanyIdQuery(IUnitOfWork unitOfWork)
+        public ContactsByCompanyIdQuery(IUnitOfWork unitOfWork, IProcess<CompanyQueryAuthorizationParams, AuthorizationResultViewModel> compAuthProcess)
         {
             _unitOfWork = unitOfWork;
+            _compAuthProcess = compAuthProcess;
         }
 
         /// <summary>
@@ -32,11 +36,26 @@ namespace MyJobLeads.DomainModel.Queries.Contacts
         }
 
         /// <summary>
+        /// Specifies the id value of the user performing the query
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public ContactsByCompanyIdQuery RequestedByUserId(int userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        /// <summary>
         /// Execute the query
         /// </summary>
         /// <returns></returns>
         public virtual IList<Contact> Execute()
         {
+            // Make sure the user has access to the company
+            if (!_compAuthProcess.Execute(new CompanyQueryAuthorizationParams { RequestingUserId = _userId, CompanyId = _companyId }).UserAuthorized)
+                return new List<Contact>();
+
             return _unitOfWork.Contacts.Fetch().Where(x => x.Company.Id == _companyId).ToList();
         }
     }
