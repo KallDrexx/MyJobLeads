67bd201baf880cad5523708776bd08f12aae9a94
mshapiro <kalldrexx@gmail.com>
2011-11-14 19:37:42 -0500

    Added BCrypt.Net and process to generate a password hash for a user

---
+++ MyJobLeads.DomainModel/Processes/Users/UserPasswordProcesses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.ViewModels.Users;
+using MyJobLeads.DomainModel.ProcessParams.Users;
+
+namespace MyJobLeads.DomainModel.Processes.Users
+{
+    public class UserPasswordProcesses : IProcess<GenerateUserPasswordHashParams, GeneratedPasswordHashViewModel>
+    {
+        public GeneratedPasswordHashViewModel Execute(GenerateUserPasswordHashParams procParams)
+        {
+            return new GeneratedPasswordHashViewModel
+            {
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(SaltPassword(procParams.Email,procParams.PlainTextPassword))
+            };
+        }
+
+        protected string SaltPassword(string email, string plainTextPassword)
+        {
+            return email + plainTextPassword;
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Processes/Users/UserPasswordProcessTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.ProcessParams.Users;
+using MyJobLeads.DomainModel.ViewModels.Users;
+using MyJobLeads.DomainModel.Data;
+using MyJobLeads.DomainModel.Processes.Users;
+
+namespace MyJobLeads.Tests.Processes.Users
+{
+    [TestClass]
+    public class UserPasswordProcessTests : EFTestBase
+    {
+        [TestMethod]
+        public void Can_Generate_BCrypt_Password_Hash_Salted_With_Email_Concatenated_With_Password()
+        {
+            // Setup
+            IProcess<GenerateUserPasswordHashParams, GeneratedPasswordHashViewModel> process = new UserPasswordProcesses();
+            string email = "email", password = "password";
+            string saltedPassword = email + password;
+
+            // Act
+            var result = process.Execute(new GenerateUserPasswordHashParams { Email = email, PlainTextPassword = password });
+
+            // Verify
+            Assert.IsNotNull(result, "Process returned a null result");
+            Assert.IsTrue(BCrypt.Net.BCrypt.Verify(saltedPassword, result.PasswordHash), "Password has was not correct");
+        }
+
+    }
+}
