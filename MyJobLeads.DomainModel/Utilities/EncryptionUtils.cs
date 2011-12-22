6230a1e0bb8100b215c28db7f19941a5fdf7f2d0
KallDrexx <me@mshapiro.net>
2011-12-22 18:44:51 -0500

    Added EncryptionUtils class

---
+++ MyJobLeads.DomainModel/Utilities/EncryptionUtils.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace MyJobLeads.DomainModel.Utilities
+{
+    public class EncryptionUtils
+    {
+        public virtual byte[] EncryptStringToBytes_AES(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the streams used
+            // to encrypt to an in memory
+            // array of bytes.
+            MemoryStream msEncrypt = null;
+            CryptoStream csEncrypt = null;
+            StreamWriter swEncrypt = null;
+
+            // Declare the RijndaelManaged object
+            // used to encrypt the data.
+            RijndaelManaged aesAlg = null;
+
+            try
+            {
+                // Create a RijndaelManaged object
+                // with the specified key and IV.
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                msEncrypt = new MemoryStream();
+                csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+                swEncrypt = new StreamWriter(csEncrypt);
+
+                //Write all data to the stream.
+                swEncrypt.Write(plainText);
+
+            }
+            finally
+            {
+                // Clean things up.
+
+                // Close the streams.
+                if (swEncrypt != null)
+                    swEncrypt.Close();
+                if (csEncrypt != null)
+                    csEncrypt.Close();
+                if (msEncrypt != null)
+                    msEncrypt.Close();
+
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return msEncrypt.ToArray();
+
+        }
+
+        public virtual string DecryptStringFromBytes_AES(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // TDeclare the streams used
+            // to decrypt to an in memory
+            // array of bytes.
+            MemoryStream msDecrypt = null;
+            CryptoStream csDecrypt = null;
+            StreamReader srDecrypt = null;
+
+            // Declare the RijndaelManaged object
+            // used to decrypt the data.
+            RijndaelManaged aesAlg = null;
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            try
+            {
+                // Create a RijndaelManaged object
+                // with the specified key and IV.
+                aesAlg = new RijndaelManaged();
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                msDecrypt = new MemoryStream(cipherText);
+                csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+                srDecrypt = new StreamReader(csDecrypt);
+
+                // Read the decrypted bytes from the decrypting stream
+                // and place them in a string.
+                plaintext = srDecrypt.ReadToEnd();
+            }
+            finally
+            {
+                // Clean things up.
+
+                // Close the streams.
+                if (srDecrypt != null)
+                    srDecrypt.Close();
+                if (csDecrypt != null)
+                    csDecrypt.Close();
+                if (msDecrypt != null)
+                    msDecrypt.Close();
+
+                // Clear the RijndaelManaged object.
+                if (aesAlg != null)
+                    aesAlg.Clear();
+            }
+
+            return plaintext;
+
+        }
+    }
+}
---
+++ MyJobLeads.Tests/Utilities/EncryptionUtilsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyJobLeads.DomainModel.Utilities;
+using System.Security.Cryptography;
+
+namespace MyJobLeads.Tests.Utilities
+{
+    [TestClass]
+    public class EncryptionUtilsTests
+    {
+        [TestMethod]
+        public void Can_Encrypt_And_Decrypt_String()
+        {
+            // Setup
+            var util = new EncryptionUtils();
+            string origString = "My String abcd123";
+            RijndaelManaged myRijndael = new RijndaelManaged();
+
+            // Act
+            var encryptedValue = util.EncryptStringToBytes_AES(origString, myRijndael.Key, myRijndael.IV);
+            string result = util.DecryptStringFromBytes_AES(encryptedValue, myRijndael.Key, myRijndael.IV);
+
+            // Verify
+            Assert.AreEqual(origString, result);
+        }
+    }
+}
