e7bb0e1151bea6199a4597cfa4a468b0259cc453
KallDrexx <me@mshapiro.net>
2011-12-22 18:20:47 -0500

    Added the jigsaw account details entity

---
+++ MyJobLeads.DomainModel/Entities/JigsawAccountDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyJobLeads.DomainModel.Entities
+{
+    public class JigsawAccountDetails
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string EncryptedPassword { get; set; }
+
+        public virtual User AssociatedUser { get; set; }
+        public int AssociatedUserId { get; set; }
+    }
+}
