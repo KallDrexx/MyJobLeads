99b4aa784f4ce901313ad7ae5600067b3f890b1d
KallDrexx <me@mshapiro.net>
2012-01-08 22:05:31 -0500

    Removed un-used DB columns

--- MyJobLeads.DomainModel/Entities/JigsawAccountDetails.cs
+++ MyJobLeads.DomainModel/Entities/JigsawAccountDetails.cs
@@ -7,11 +7,10 @@ namespace MyJobLeads.DomainModel.Entities
 {
     public class JigsawAccountDetails
     {
-        public int Id { get; set; }
+        public int Id { get; set; } // This will equal the id value of the user it's associated with
         public string Username { get; set; }
         public string EncryptedPassword { get; set; }
 
         public virtual User AssociatedUser { get; set; }
-        public int AssociatedUserId { get; set; }
     }
 }
