194ecd2ac2b6a30b9243a46d1c3a5fb9b4437976
KallDrexx <me@mshapiro.net>
2012-01-16 22:21:14 -0500

    Added process params, view models, and json for jigsaw company searching

---
+++ MyJobLeads.DomainModel/ProcessParams/CompanySearching/Jigsaw/JigsawCompanySearchParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyJobLeads.DomainModel.ProcessParams.CompanySearching.Jigsaw
+{
+    public class JigsawCompanySearchParams
+    {
+        public int RequestedPageNum { get; set; }
+        public string CompanyName { get; set; }
+        public string Industry { get; set; }
+        public string MetroArea { get; set; }
+        public string Employees { get; set; }
+        public string Revenue { get; set; }
+        public string Ownership { get; set; }
+    }
+}
---
+++ MyJobLeads.DomainModel/ViewModels/CompanySearching/ExternalCompanySearchResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyJobLeads.DomainModel.ViewModels.CompanySearching
+{
+    public class ExternalCompanySearchResultViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
