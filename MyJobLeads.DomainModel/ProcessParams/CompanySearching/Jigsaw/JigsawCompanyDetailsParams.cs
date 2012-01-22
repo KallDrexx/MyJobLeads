50c0cedb2e252e56d8edaacbb9c5593c48859945
KallDrexx <me@mshapiro.net>
2012-01-22 08:45:21 -0500

    Updated json and added new view models

---
+++ MyJobLeads.DomainModel/ProcessParams/CompanySearching/Jigsaw/JigsawCompanyDetailsParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyJobLeads.DomainModel.ProcessParams.CompanySearching.Jigsaw
+{
+    public class JigsawCompanyDetailsParams
+    {
+        public int RequestingUserId { get; set; }
+        public int JigsawId { get; set; }
+    }
+}
