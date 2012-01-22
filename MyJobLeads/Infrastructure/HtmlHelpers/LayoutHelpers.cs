420275bc2e7f849a121a46f5f1af8d23c17dfca9
KallDrexx <me@mshapiro.net>
2012-01-22 09:14:04 -0500

    Created the layout helper with the page info box writer

---
+++ MyJobLeads/Infrastructure/HtmlHelpers/LayoutHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyJobLeads.Infrastructure.HtmlHelpers.Layouts;
+using System.Web.Mvc;
+
+namespace MyJobLeads.Infrastructure.HtmlHelpers
+{
+    public static class LayoutHelpers
+    {
+        public static PageInfoBoxWriter PageInfoBox(this HtmlHelper html, bool includeSeparator)
+        {
+            return new PageInfoBoxWriter(html.ViewContext, includeSeparator);
+        }
+    }
+}
