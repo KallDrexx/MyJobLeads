c53f31ad468eef99f35d6b7c2474584daaf96a67
KallDrexx <me@mshapiro.net>
2012-01-28 11:41:16 -0500

    Created the company search MVC area

---
+++ MyJobLeads/Areas/CompanySearch/CompanySearchAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MyJobLeads.Areas.CompanySearch
+{
+    public class CompanySearchAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "CompanySearch";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "CompanySearch_default",
+                "CompanySearch/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
