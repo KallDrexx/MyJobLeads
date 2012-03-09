0f41e54f2a4b87651420e1d7d1072dff86f5ad7e
KallDrexx <me@mshapiro.net>
2012-03-09 00:18:16 -0800

    Added IProcess.cs that got missed, updated project settings to be more globally friendly

---
+++ MyJobLeads.DomainModel/Data/IProcess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyJobLeads.DomainModel.Data
+{
+    public interface IProcess<TProcessParams, TViewModel>
+    {
+        TViewModel Execute(TProcessParams procParams);
+    }
+}
